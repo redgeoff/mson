f8d186bf52d2e8347aeff1edf0164df0f7bb2a9d
Geoff Cox <redgeoff@gmail.com>
2018-03-07 16:43:10 -0800

    initial

---
+++ src/lab/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        mson lab
+      </div>
+    );
+  }
+}
+
+export default App;
