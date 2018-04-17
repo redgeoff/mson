6c020c34699cff423edeca7d36d227e9ebd21797
Geoff Cox <redgeoff@gmail.com>
2018-04-17 09:33:25 -0700

    refactor(object-form): split out

--- src/mson/form-validator.js
+++ src/mson/form-validator.js
@@ -1,35 +1,9 @@
 import Form from './form';
 import { TextField, FormField } from './fields';
 import sift from 'sift';
+import ObjectForm from './object-form';
 
-// TODO: Abstract ValidatorSelector into something like ObjectField so it can be used to define
-// generic objects?
-class ValidatorSelector extends Form {
-  constructor(props) {
-    super(props);
-
-    this._selectorError = null;
-  }
-
-  _create(props) {
-    super._create(Object.assign(props, { omitDefaultFields: true }));
-
-    // The fields are dynamic so we need to disable the undefined check
-    this._set('reportUndefined', false);
-  }
-
-  setValues(values) {
-    super.setValues(values);
-
-    this._selectorError = null;
-    this._valueSet = values;
-  }
-
-  // Needed as
-  isBlank() {
-    return this._valueSet ? false : true;
-  }
-
+class ValidatorSelector extends ObjectForm {
   validate() {
     super.validate();
 
@@ -38,18 +12,10 @@ class ValidatorSelector extends Form {
         // Use sift to validate the selector
         sift(this._valueSet);
       } catch (err) {
-        this._selectorError = err.message;
+        this._errorFromSet = err.message;
         this.set({ err: true });
       }
     }
-  }
-
-  getErrs() {
-    let errs = super.getErrs();
-    if (this._selectorError !== null) {
-      errs = errs.concat({ error: this._selectorError });
-    }
-    return errs;
   }
 }
 
