c22a5f8cd6f5210fedcbae6da66994d93496d49f
Geoff Cox <redgeoff@gmail.com>
2018-04-21 12:32:36 -0700

    refactor(client): move to mson-server

--- src/mson/client/api.js
+++
@@ -1,9 +0,0 @@
-// import App from './app';
-import Builder from './builder';
-
-export default class API {
-  constructor(client) {
-    // this.app = new App(client);
-    this.builder = new Builder(client);
-  }
-}
--- src/mson/client/builder/index.js
+++
@@ -1,7 +0,0 @@
-import User from './user';
-
-export default class Builder {
-  constructor(client) {
-    this.user = new User(client);
-  }
-}
