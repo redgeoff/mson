3253b3d4153516105d88cd9a11a7b6466ce485f5
Geoff Cox <redgeoff@gmail.com>
2018-05-04 15:26:52 -0700

    feat(test): use wait-on

--- test.sh
+++ test.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
 yarn test-server-start
+
+# Wait for test-server to start
+./node_modules/wait-on/bin/wait-on tcp:4002
+
 react-scripts test --env=jsdom $@
 code=$?
+
 yarn test-server-stop
 
 if [ $code -ne 0 ];
