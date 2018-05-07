704c57d4af294557cf3ef6c5b754547b60ee8f01
Geoff Cox <redgeoff@gmail.com>
2018-05-07 16:58:50 -0700

    doc(user): clean up comments

--- src/mson/form/user.js
+++ src/mson/form/user.js
@@ -1,52 +1,7 @@
 import Form from './form';
-// import _ from 'lodash';
-// import { EmailField, PasswordField } from '../fields';
-// import { MSONComponent } from '../fields';
 import MSONComponent from '../component/mson-component';
 
 export default class User extends Form {
-  //   _create(props) {
-  //     // Clone so that we don't modify original data
-  //     props = _.clone(props);
-  //
-  //     const fields = [
-  //       new EmailField({
-  //         name: 'username',
-  //         label: 'Username',
-  //         required: true
-  //       }),
-  //       new PasswordField({
-  //         name: 'password',
-  //         label: 'Password',
-  //         required: true
-  //       })
-  //     ];
-  //
-  //     if (props.fields) {
-  //       props.fields = fields.concat(props.fields);
-  // console.log('HAS FIELDS', props.fields);
-  //     } else {
-  // console.log('NO FIELDS');
-  //       props.fields = fields;
-  //     }
-  //
-  //     super._create(props);
-  //   }
-
-  // _createDefaultFields() {
-  //   super._createDefaultFields();
-  //   // this.addField(new EmailField({
-  //   //   name: 'username',
-  //   //   label: 'Username',
-  //   //   required: true
-  //   // }));
-  //   // this.addField(new PasswordField({
-  //   //   name: 'password',
-  //   //   label: 'Password',
-  //   //   required: true
-  //   // }));
-  // }
-
   _createDefaultFields() {
     super._createDefaultFields();
 
@@ -83,45 +38,3 @@ export default class User extends Form {
     return value === undefined ? super.getOne(name) : value;
   }
 }
-
-// export default class User extends Form {
-//   _create(props) {
-//     // Clone so that we don't modify original data
-//     props = _.clone(props);
-//
-//     const fields = [
-//       {
-//         component: 'EmailField',
-//         name: 'username',
-//         label: 'Username',
-//         required: true
-//       },
-//       {
-//         component: 'PasswordField',
-//         name: 'password',
-//         label: 'Password',
-//         required: true
-//       }
-//     ];
-//
-//     if (props.fields) {
-//       props.fields = fields.concat(props.fields);
-// console.log('HAS FIELDS', props.fields);
-//     } else {
-// console.log('NO FIELDS');
-//       props.fields = fields;
-//     }
-//
-//     super._create(props);
-//   }
-//
-//   set(props) {
-//     super.set(props);
-//     this._setIfUndefined(props, 'roles');
-//   }
-//
-//   getOne(name) {
-//     const value = this._getIfAllowed(name, 'roles');
-//     return value === undefined ? super.getOne(name) : value;
-//   }
-// }
