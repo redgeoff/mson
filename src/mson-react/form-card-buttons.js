4ea8d9bbdf2b156ffab61306a8f1d7b80a3b2c9f
Geoff Cox <redgeoff@gmail.com>
2018-06-06 11:14:50 -0700

    refactor(form-card-buttons): disable tooltip to optimize

---
+++ src/mson-react/form-card-buttons.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import IconButton from '@material-ui/core/IconButton';
+// import MoreVertIcon from '@material-ui/icons/MoreVert';
+// import Menu, { MenuItem } from '@material-ui/core/Menu';
+// import { ListItemIcon, ListItemText } from '@material-ui/core/List';
+import { ModeEdit, Delete, Restore } from '@material-ui/icons';
+import Grid from '@material-ui/core/Grid';
+// import { Tooltip } from '@material-ui/core';
+
+export default class FormCardButtons extends React.PureComponent {
+  render() {
+    const {
+      forbidUpdate,
+      forbidDelete,
+      editable,
+      disabled,
+      archivedAt
+    } = this.props;
+
+    // NOTE: using Tooltips below when we have 100 more items leads to a significant latency. In the
+    // future, if we wish to support tooltips then we'll probably need to lazy load the tooltip on
+    // mouse over.
+
+    let updateButton = null;
+    if (!forbidUpdate) {
+      // <Tooltip title="Edit">
+      updateButton = (
+        <IconButton onClick={event => this.handleEdit(event)}>
+          <ModeEdit />
+        </IconButton>
+      );
+      // </Tooltip>
+    }
+
+    let deleteButton = null;
+    if (!forbidDelete) {
+      deleteButton = (
+        <IconButton onClick={event => this.handleDelete(event)}>
+          {archivedAt ? <Restore /> : <Delete />}
+        </IconButton>
+      );
+    }
+
+    if (editable && !disabled && (!forbidUpdate || !forbidDelete)) {
+      return (
+        <Grid item>
+          {updateButton}
+          {deleteButton}
+          {/* TODO: make the more menu optional
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={this.handleMoreClose}
+          >
+            <MenuItem
+              className={classes.menuItem}
+              onClick={event => this.handleEdit(event)}
+            >
+              <ListItemIcon className={classes.icon}>
+                <ModeEdit />
+              </ListItemIcon>
+              <ListItemText
+                classes={{ primary: classes.primary }}
+                inset
+                primary="Edit"
+              />
+            </MenuItem>
+            <MenuItem
+              className={classes.menuItem}
+              onClick={event => this.handleDelete(event)}
+            >
+              <ListItemIcon className={classes.icon}>
+                <Delete />
+              </ListItemIcon>
+              <ListItemText
+                classes={{ primary: classes.primary }}
+                inset
+                primary="Delete"
+              />
+            </MenuItem>
+          </Menu>
+          */}
+        </Grid>
+      );
+    } else {
+      return null;
+    }
+  }
+}
--- src/mson-react/form-card.js
+++ src/mson-react/form-card.js
@@ -1,17 +1,10 @@
-// TODO: make the more menu optional
-
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import IconButton from '@material-ui/core/IconButton';
-// import MoreVertIcon from '@material-ui/icons/MoreVert';
-// import Menu, { MenuItem } from '@material-ui/core/Menu';
-// import { ListItemIcon, ListItemText } from '@material-ui/core/List';
-import { ModeEdit, Delete, Restore } from '@material-ui/icons';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Form from './form';
-import { Tooltip } from '@material-ui/core';
 import attach from './attach';
+import FormCardButtons from './form-card-buttons';
 
 const styles = theme => ({
   paper: {
@@ -26,18 +19,6 @@ const styles = theme => ({
 
 // Use PureComponent so that we avoid unnecessary re-rendering
 class FormCard extends React.PureComponent {
-  // state = {
-  //   anchorEl: null
-  // };
-
-  // handleMoreClick = event => {
-  //   this.setState({ anchorEl: event.currentTarget });
-  // };
-  //
-  // handleMoreClose = () => {
-  //   this.setState({ anchorEl: null });
-  // };
-
   handleClick = event => {
     if (this.props.onClick) {
       this.props.onClick(event, this.props.form);
@@ -48,7 +29,6 @@ class FormCard extends React.PureComponent {
     if (this.props.onEdit) {
       this.props.onEdit(event, this.props.form);
     }
-    // this.handleMoreClose();
   };
 
   handleDelete = event => {
@@ -68,69 +48,6 @@ class FormCard extends React.PureComponent {
       disabled,
       archivedAt
     } = this.props;
-    // const { anchorEl } = this.state;
-
-    let buttons = null;
-    const deleteTitle = archivedAt ? 'Restore' : 'Delete';
-    if (editable && !disabled && (!forbidUpdate || !forbidDelete)) {
-      buttons = (
-        <Grid item>
-          {forbidUpdate ? (
-            ''
-          ) : (
-            <Tooltip title="Edit">
-              <IconButton onClick={event => this.handleEdit(event)}>
-                <ModeEdit />
-              </IconButton>
-            </Tooltip>
-          )}
-          {forbidDelete ? (
-            ''
-          ) : (
-            <Tooltip title={deleteTitle}>
-              <IconButton onClick={event => this.handleDelete(event)}>
-                {archivedAt ? <Restore /> : <Delete />}
-              </IconButton>
-            </Tooltip>
-          )}
-          {/* TODO: make the more menu optional
-          <Menu
-            id="simple-menu"
-            anchorEl={anchorEl}
-            open={Boolean(anchorEl)}
-            onClose={this.handleMoreClose}
-          >
-            <MenuItem
-              className={classes.menuItem}
-              onClick={event => this.handleEdit(event)}
-            >
-              <ListItemIcon className={classes.icon}>
-                <ModeEdit />
-              </ListItemIcon>
-              <ListItemText
-                classes={{ primary: classes.primary }}
-                inset
-                primary="Edit"
-              />
-            </MenuItem>
-            <MenuItem
-              className={classes.menuItem}
-              onClick={event => this.handleDelete(event)}
-            >
-              <ListItemIcon className={classes.icon}>
-                <Delete />
-              </ListItemIcon>
-              <ListItemText
-                classes={{ primary: classes.primary }}
-                inset
-                primary="Delete"
-              />
-            </MenuItem>
-          </Menu>
-          */}
-        </Grid>
-      );
-    }
 
     return (
       <div>
@@ -143,7 +60,13 @@ class FormCard extends React.PureComponent {
             >
               <Form form={form} formTag={false} mode="read" />
             </Grid>
-            {buttons}
+            <FormCardButtons
+              forbidUpdate={forbidUpdate}
+              forbidDelete={forbidDelete}
+              editable={editable}
+              disabled={disabled}
+              archivedAt={archivedAt}
+            />
           </Grid>
         </Paper>
       </div>
