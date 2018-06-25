d1f882fdbf38dabe29474a78b3fe608dc6be77ec
Geoff Cox <redgeoff@gmail.com>
2018-06-25 11:33:02 -0700

    refactor(employees): move set password logic to forms field

--- src/employees/components.js
+++ src/employees/components.js
@@ -25,91 +25,10 @@ export const employee = {
       options: [
         { value: 'admin', label: 'Admin' },
         { value: 'manager', label: 'Manager' }
-      ],
-      before: 'setPassword'
-    },
-    {
-      component: 'ButtonField',
-      name: 'setPassword',
-      label: 'Set Password',
-      hidden: true
+      ]
     }
   ],
-
-  roles: ['employee'],
-
-  listeners: [
-    {
-      event: 'beginCreate',
-      actions: [
-        {
-          component: 'Set',
-          name: 'fields.password.hidden',
-          value: false
-        },
-        {
-          component: 'Set',
-          name: 'fields.password.out',
-          value: true
-        },
-        {
-          component: 'Set',
-          name: 'fields.password.required',
-          value: true
-        }
-      ]
-    },
-    {
-      event: 'endCreate',
-      actions: [
-        {
-          component: 'Set',
-          name: 'fields.password.hidden',
-          value: true
-        },
-        {
-          component: 'Set',
-          name: 'fields.password.out',
-          value: false
-        },
-        {
-          component: 'Set',
-          name: 'fields.password.required',
-          value: false
-        }
-      ]
-    },
-    {
-      event: 'beginRead',
-      actions: [
-        {
-          component: 'Set',
-          name: 'fields.setPassword.hidden',
-          value: false
-        }
-      ]
-    },
-    {
-      event: 'endRead',
-      actions: [
-        {
-          component: 'Set',
-          name: 'fields.setPassword.hidden',
-          value: true
-        }
-      ]
-    },
-    {
-      event: 'setPassword',
-      actions: [
-        {
-          component: 'Set',
-          name: 'parent.mode',
-          value: 'update'
-        }
-      ]
-    }
-  ]
+  roles: ['employee']
 };
 
 export const department = {
--- src/lab/mson-app.js
+++ src/lab/mson-app.js
@@ -355,7 +355,87 @@ compiler.registerComponent('app.Employees', {
       label: 'Employees',
       component: 'FormsField',
       form: {
-        component: 'app.Employee'
+        component: 'app.Employee',
+        fields: [
+          {
+            component: 'ButtonField',
+            name: 'setPassword',
+            label: 'Set Password',
+            hidden: true
+          }
+        ],
+        listeners: [
+          {
+            event: 'beginCreate',
+            actions: [
+              {
+                component: 'Set',
+                name: 'fields.password.hidden',
+                value: false
+              },
+              {
+                component: 'Set',
+                name: 'fields.password.out',
+                value: true
+              },
+              {
+                component: 'Set',
+                name: 'fields.password.required',
+                value: true
+              }
+            ]
+          },
+          {
+            event: 'endCreate',
+            actions: [
+              {
+                component: 'Set',
+                name: 'fields.password.hidden',
+                value: true
+              },
+              {
+                component: 'Set',
+                name: 'fields.password.out',
+                value: false
+              },
+              {
+                component: 'Set',
+                name: 'fields.password.required',
+                value: false
+              }
+            ]
+          },
+          {
+            event: 'beginRead',
+            actions: [
+              {
+                component: 'Set',
+                name: 'fields.setPassword.hidden',
+                value: false
+              }
+            ]
+          },
+          {
+            event: 'endRead',
+            actions: [
+              {
+                component: 'Set',
+                name: 'fields.setPassword.hidden',
+                value: true
+              }
+            ]
+          },
+          {
+            event: 'setPassword',
+            actions: [
+              {
+                component: 'Set',
+                name: 'parent.mode',
+                value: 'update'
+              }
+            ]
+          }
+        ]
       },
       store: {
         component: 'RecordStore',
