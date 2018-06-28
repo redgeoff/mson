e0929a0a7bc45a01e56c3b40a727897788b2f48e
Geoff Cox <redgeoff@gmail.com>
2018-06-28 12:27:48 -0700

    fix(employees): redirect on cancel change password

--- src/lab/mson-app.js
+++ src/lab/mson-app.js
@@ -218,7 +218,7 @@ compiler.registerComponent('app.ChangePasswordForm', {
       ]
     },
     {
-      event: 'saved',
+      event: ['saved', 'cancel'],
       actions: [
         {
           component: 'Redirect',
