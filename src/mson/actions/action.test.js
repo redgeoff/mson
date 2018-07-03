a9ad1dae005a8325d1c35c74f415c666e5a34e6c
Geoff Cox <redgeoff@gmail.com>
2018-07-02 17:25:25 -0700

    test(action): multiple actions

--- src/mson/actions/action.test.js
+++ src/mson/actions/action.test.js
@@ -1,4 +1,5 @@
 import Set from './set';
+import Action from './action';
 import TextField from '../fields/text-field';
 
 const createAction = () => {
@@ -16,11 +17,36 @@ const createAction = () => {
   });
 };
 
+const createActions = () => {
+  return new Action({
+    actions: [
+      new Set({
+        name: 'value',
+        value: 'Jack'
+      }),
+      new Set({
+        name: 'hidden',
+        value: true
+      })
+    ]
+  });
+};
+
 it('should act', async () => {
   const action = createAction();
-  const field = new TextField({ name: 'firstName ' });
+  const field = new TextField({ name: 'firstName ' }); // Note: trailing space on purpose
   await action.run({
     component: field
   });
   expect(field.getValue()).toEqual('Jack');
+});
+
+it('should perform multiple actions', async () => {
+  const actions = createActions();
+  const field = new TextField({ name: 'firstName' });
+  await actions.run({
+    component: field
+  });
+  expect(field.getValue()).toEqual('Jack');
+  expect(field.get('hidden')).toEqual(true);
 });
