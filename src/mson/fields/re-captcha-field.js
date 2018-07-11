241fd3c4c70fd80861d3830e9d1d69d1a898558d
Geoff Cox <redgeoff@gmail.com>
2018-07-11 06:32:09 -0700

    fix(re-captcha-field): name is not required

--- src/mson/fields/re-captcha-field.js
+++ src/mson/fields/re-captcha-field.js
@@ -4,8 +4,22 @@ export default class ReCAPTCHAField extends Field {
   _create(props) {
     super._create(props);
 
-    this._setDefaults(props, {
+    this.set({
       name: 'captcha',
+      schema: {
+        component: 'Form',
+        fields: [
+          {
+            // Name is not required
+            name: 'name',
+            component: 'TextField',
+            required: false
+          }
+        ]
+      }
+    });
+
+    this._setDefaults(props, {
       required: true
     });
   }
