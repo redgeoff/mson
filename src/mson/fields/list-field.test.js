b6a7da2d4ee302b4116ee40dec0ebe1400464239
Geoff Cox <redgeoff@gmail.com>
2018-08-29 20:05:55 -0700

    test(list-field): more coverage

--- src/mson/fields/list-field.test.js
+++ src/mson/fields/list-field.test.js
@@ -75,3 +75,44 @@ it('should allow scalar values', () => {
   expect(field.hasErr()).toEqual(false);
   expect(field.getValue()).toEqual(['one']); // still an array
 });
+
+it('should handle dirty factory', () => {
+  const field = new TextListField();
+
+  const setSpy = jest.spyOn(field, 'set');
+
+  field._handleDirtyFactory()(false);
+  expect(setSpy).toHaveBeenCalledTimes(0);
+
+  field._handleDirtyFactory()(true);
+  expect(setSpy).toHaveBeenCalledWith({ dirty: true });
+});
+
+it('should set block', () => {
+  const field = new TextListField({ value: ['red'] });
+
+  field.set({ block: true });
+  expect(field.get('block')).toEqual(true);
+  field.eachField(field => expect(field.get('block')).toEqual(true));
+
+  field.set({ block: false });
+  expect(field.get('block')).toEqual(false);
+  field.eachField(field => expect(field.get('block')).toEqual(false));
+});
+
+it('should set full width', () => {
+  const field = new TextListField({ value: ['red'] });
+
+  field.set({ fullWidth: true });
+  expect(field.get('fullWidth')).toEqual(true);
+  field.eachField(field => expect(field.get('fullWidth')).toEqual(true));
+
+  field.set({ fullWidth: false });
+  expect(field.get('fullWidth')).toEqual(false);
+  field.eachField(field => expect(field.get('fullWidth')).toEqual(false));
+});
+
+it('new field should throw when field not defined', () => {
+  const field = new ListField();
+  expect(() => field._newField(0)).toThrow();
+});
