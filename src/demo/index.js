ea93a7dcb964edfbc5f12d8d642683370b3b2fad
Geoff Cox <redgeoff@gmail.com>
2018-09-01 08:25:52 -0700

    refactor(demo): restore

--- src/demo/index.js
+++ src/demo/index.js
@@ -1,25 +1,8 @@
 // Note: babel-polyfill is needed by client for compilation
 import 'babel-polyfill';
 
-// import app from './app';
-// import mson from '../mson-react';
-//
-// // Render the app
-// mson.render(app);
+import app from './app';
+import mson from '../mson-react';
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-// import FlexBreak from '../mson-react/flex-break';
-// ReactDOM.render(<FlexBreak />, document.getElementById('root'));
-
-// import TextField from '../mson/fields/text-field';
-// import TextFieldUI from '../mson-react/fields/text-field';
-// const field = new TextField({ name: 'firstName', label: 'First Name' });
-// ReactDOM.render(<TextFieldUI field={field} />, document.getElementById('root'));
-
-import Button from '../mson-react/button';
-// import Icon from '../mson-react/icon';
-ReactDOM.render(<Button icon="AddCircle" />, document.getElementById('root'));
-// ReactDOM.render(<Button icon="Save" />, document.getElementById('root'));
-// ReactDOM.render(<Icon icon="Info" />, document.getElementById('root'));
+// Render the app
+mson.render(app);
--- src/demo/index.orig.js
+++
@@ -1,8 +0,0 @@
-// Note: babel-polyfill is needed by client for compilation
-import 'babel-polyfill';
-
-import app from './app';
-import mson from '../mson-react';
-
-// Render the app
-mson.render(app);
