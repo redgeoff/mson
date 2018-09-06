93b5485bba6837e9f0d66dabf5f291a054683ebb
Geoff Cox <redgeoff@gmail.com>
2018-09-05 17:45:54 -0700

    test(local-storage): implementation for node

--- src/mson/stores/local-storage-store.js
+++ src/mson/stores/local-storage-store.js
@@ -1,10 +1,11 @@
 import MemoryStore from './memory-store';
+import localStorage from './local-storage';
 
 export default class LocalStorageStore extends MemoryStore {
   _className = 'LocalStorageStore';
 
   _loadItems(storeName) {
-    const store = localStorage.getItem(storeName);
+    const store = this._localStorage.getItem(storeName);
     if (store !== null) {
       const items = JSON.parse(store);
       items.forEach(item => {
@@ -13,8 +14,21 @@ export default class LocalStorageStore extends MemoryStore {
     }
   }
 
+  // Allow for testing in Node
+  _getLocalStorage() {
+    if (this._global.window && this._global.window.localStorage) {
+      return this._global.window.localStorage;
+    } else {
+      return localStorage;
+    }
+  }
+
   _create(props) {
     super._create(props);
+
+    // For mocking
+    this._global = global;
+    this._localStorage = this._getLocalStorage();
 
     this.set({
       schema: {
@@ -33,7 +47,7 @@ export default class LocalStorageStore extends MemoryStore {
 
   _saveItems() {
     const items = this._items.map(item => item);
-    localStorage.setItem(this.get('storeName'), JSON.stringify(items));
+    this._localStorage.setItem(this.get('storeName'), JSON.stringify(items));
   }
 
   async _doAndSave(promiseFactory) {
