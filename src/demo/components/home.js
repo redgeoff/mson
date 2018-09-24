c5c60dd25610f5e3afb00bad6f588d364fd6b2c3
Geoff Cox <redgeoff@gmail.com>
2018-09-24 06:36:43 -0700

    feat(text): support markdown

--- src/demo/components/home.js
+++ src/demo/components/home.js
@@ -1,5 +1,5 @@
 export default {
   name: 'app.Home',
   component: 'Text',
-  text: 'Welcome'
+  text: '# Welcome\nCheck out the examples on the left'
 };
--- src/mson-react/text.js
+++ src/mson-react/text.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import attach from './attach';
 import Typography from '@material-ui/core/Typography';
+import ReactMarkdown from 'react-markdown';
 
 class Text extends React.PureComponent {
   render() {
     const { text } = this.props;
 
-    return <Typography>{text}</Typography>;
+    // We use component=div to force usage of a div, instead of a p, as properly formatted HTML
+    // cannot nest tags like h1 under a <p>.
+    return (
+      <Typography component="div">
+        <ReactMarkdown source={text} />
+      </Typography>
+    );
   }
 }
 
