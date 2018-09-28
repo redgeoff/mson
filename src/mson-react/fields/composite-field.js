b9b25ad39537db8f7dab22dd61ad642ba22d955e
Geoff Cox <redgeoff@gmail.com>
2018-09-27 20:32:23 -0700

    fix(composite-field): skip empty on display

--- src/mson-react/fields/composite-field.js
+++ src/mson-react/fields/composite-field.js
@@ -6,17 +6,21 @@ import FlexBreak from '../flex-break';
 
 class CompositeField extends React.PureComponent {
   render() {
-    const { component, help, editable } = this.props;
+    const { component, help, editable, useDisplayValue } = this.props;
 
     let fields = [];
 
     let lastIsBlock = false;
 
     component.eachField((field, index, last) => {
-      if (last && field.get('block')) {
-        lastIsBlock = true;
+      // Don't show the field if we are using the display value and it is blank, e.g. it is the
+      // empty "next" field.
+      if (!useDisplayValue || !field.isBlank()) {
+        if (last && field.get('block')) {
+          lastIsBlock = true;
+        }
+        fields.push(<Field component={field} key={index} noBlock={last} />);
       }
-      fields.push(<Field component={field} key={index} noBlock={last} />);
     });
 
     if (help && editable) {
@@ -33,4 +37,6 @@ class CompositeField extends React.PureComponent {
 }
 
 // We want the component to update when we receive new fields
-export default attach(['change', 'help', 'editable'])(CompositeField);
+export default attach(['change', 'help', 'editable', 'useDisplayValue'])(
+  CompositeField
+);
