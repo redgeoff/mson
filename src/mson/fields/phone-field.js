61f1ea02f2a474915d8888e49ba1a531bb15771e
Geoff Cox <redgeoff@gmail.com>
2018-10-01 07:13:08 -0700

    feat(phone-number): use country telephone data

--- src/mson/fields/phone-field.js
+++ src/mson/fields/phone-field.js
@@ -1,23 +1,48 @@
 import TextField from './text-field';
 import map from 'lodash/map';
+import countryTelephoneData from 'country-telephone-data';
 
 export default class PhoneField extends TextField {
   _className = 'PhoneField';
 
-  // Convert from react-telephone-input to text-mask mask
+  // Convert from country-telephone-data to text-mask mask
   _toMask(reactTelMask) {
     return map(reactTelMask, item => (item === '.' ? /\d/ : item));
   }
 
+  static getCountriesByCode() {
+    if (!this.constructor._countriesByCode) {
+      this.constructor._countriesByCode = {};
+      const countriesByCode = this.constructor._countriesByCode;
+      countryTelephoneData.allCountries.forEach(
+        country => (countriesByCode[country.dialCode] = country)
+      );
+    }
+    return this.constructor._countriesByCode;
+  }
+
   _maskValue(value) {
     if (value) {
+      const countries = this.constructor.getCountriesByCode();
       if (value[0] === '+') {
-        if (value.indexOf('+1') === 0) {
-          return this._toMask('+. (...) ...-....');
-        } else if (value.indexOf('+44') === 0) {
-          return this._toMask('+.. .... ......');
+        // Single-digit match
+        let country = countries[value[1]];
+
+        // Double-digit match
+        if (!country) {
+          country = countries[[value[1], value[2]].join('')];
+        }
+
+        // Triple-digit match
+        if (!country) {
+          country = countries[[value[1], value[2], value[3]].join('')];
+        }
+
+        if (country) {
+          return this._toMask(country.format);
         }
       } else {
+        // TODO: make this customizable, but default to US without country code
         return this._toMask('(...) ...-....');
       }
     }
