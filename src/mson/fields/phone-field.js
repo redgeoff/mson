5294e75728dd4db6b725121c7a67f2e3d4ce43a2
Geoff Cox <redgeoff@gmail.com>
2018-10-01 10:51:57 -0700

    fix(phone-field): clearin in ui

--- src/mson/fields/phone-field.js
+++ src/mson/fields/phone-field.js
@@ -2,6 +2,8 @@ import TextField from './text-field';
 import map from 'lodash/map';
 import countryTelephoneData from 'country-telephone-data';
 
+const EMPTY_MASK = [/./, /./, /./, /./];
+
 export default class PhoneField extends TextField {
   _className = 'PhoneField';
 
@@ -45,9 +47,16 @@ export default class PhoneField extends TextField {
       }
     }
 
+    // When returning false, there appears to be a bug in MaskedInput (react layer) that causes
+    // issues when clearing the input, i.e. the old value is still present. As a workaround, we'll
+    // return a mask that allows the user to enter any four characters--enough to determine the
+    // final mask.
+    //
     // Disable mask so that user can user can enter the first few characters, which will determine
     // the applicable mask
-    return false;
+    // return false;
+    //
+    return EMPTY_MASK;
   }
 
   _create(props) {
@@ -74,7 +83,7 @@ export default class PhoneField extends TextField {
   toConformedValue(value) {
     const mask = this._getMask(value);
     // Is there a mask in which to conform?
-    if (mask) {
+    if (mask !== EMPTY_MASK) {
       return super.toConformedValue(value);
     } else {
       return value;
@@ -89,10 +98,7 @@ export default class PhoneField extends TextField {
       clonedProps.defaultMask = this._formatMask(clonedProps.defaultMask);
     }
 
-    if (
-      clonedProps.value !== undefined &&
-      !this.isValueBlank(clonedProps.value)
-    ) {
+    if (clonedProps.value !== undefined) {
       clonedProps.value = this.toConformedValue(clonedProps.value);
     }
 
@@ -119,7 +125,7 @@ export default class PhoneField extends TextField {
     if (!this.hasErr() && !this.isBlank()) {
       const value = this.getValue();
       let mask = this._getMask(value);
-      if (!mask || !this._conformsToMask(value, mask)) {
+      if (mask === EMPTY_MASK || !this._conformsToMask(value, mask)) {
         this.setErr('invalid');
       }
     }
