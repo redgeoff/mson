c8ecbae1688349ad831917886fcf83c8ce55b4d7
Geoff Cox <redgeoff@gmail.com>
2018-10-10 16:34:40 -0700

    refactor(css-loader): not needed

--- webpack.config.js
+++ webpack.config.js
@@ -18,10 +18,6 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
       }
     ]
   }
