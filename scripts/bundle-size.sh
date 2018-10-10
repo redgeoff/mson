9d6c2dccf4ce015c61d332add138e51a022a482b
Geoff Cox <redgeoff@gmail.com>
2018-10-10 16:37:32 -0700

    refactor(bundle-size): use webpack

--- scripts/bundle-size.sh
+++ scripts/bundle-size.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 
-mainMaxBytes=280000
-chunkMaxBytes=940000
+maxBytes=270000
 
-if [ $(wc -c < build/static/js/main.*.js) -gt ${mainMaxBytes} ]; then
-  echo 'Error: main bundle too large!'
-  exit 1
-fi
-
-if [ $(wc -c < build/static/js/1.*.chunk.js) -gt ${chunkMaxBytes} ]; then
-  echo 'Error: chunk bundle too large!'
+if [ $(wc -c < dist/mson.js) -gt ${maxBytes} ]; then
+  echo 'Error: bundle too large!'
   exit 1
 fi
