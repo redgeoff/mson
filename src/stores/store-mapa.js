669aa6c660717d7ad0463072b7131a6c14dc6df4
Geoff Cox <redgeoff@gmail.com>
2018-10-24 14:36:30 -0700

    feat(store-mapa): reordering

--- src/stores/store-mapa.js
+++ src/stores/store-mapa.js
@@ -6,6 +6,10 @@ export default class StoreMapa extends Mapa {
     return doc[name];
   }
 
+  _setProp(doc, name, value) {
+    return (doc[name] = value);
+  }
+
   // FUTURE: for small data sets, the only ones we expect to use with the ordering, it is relatively
   // quick to iterate through the items to find where to insert an item. If a speed increase is
   // needed, we could use a b-tree to find the correct place in logrithmic time.
@@ -16,6 +20,20 @@ export default class StoreMapa extends Mapa {
         entryOrder === undefined ||
         entryOrder === null ||
         entryOrder > order
+      ) {
+        return entry[0];
+      }
+    }
+    return null;
+  }
+
+  _findBeforeId(order) {
+    for (const entry of this.entries()) {
+      const entryOrder = this._getProp(entry[1], 'order');
+      if (
+        entryOrder === undefined ||
+        entryOrder === null ||
+        entryOrder >= order
       ) {
         return entry[0];
       }
@@ -39,32 +57,211 @@ export default class StoreMapa extends Mapa {
     }
   }
 
-  _createInDocs(id, doc, beforeId) {
-    if (beforeId === undefined) {
+  _createInDocs(id, doc) {
+    const beforeId = this.getBeforeId(doc);
+    return super.set(id, doc, beforeId);
+  }
+
+  _createInDocsAndReorder(id, doc) {
+    let order = this._getProp(doc, 'order');
+    let beforeId = undefined;
+    if (order !== undefined && order !== null) {
+      beforeId = this._findBeforeId(order);
+    }
+
+    const setDoc = super.set(id, doc, beforeId);
+
+    if (beforeId !== null && beforeId !== undefined) {
+      // Update subsequent orders
+      for (const entry of this.entries(setDoc.nextKey)) {
+        const value = entry[1];
+        // TODO: need onReorder
+        this._setProp(value, 'order', ++order);
+        super.set(entry[0], value);
+      }
+    }
+
+    return setDoc;
+  }
+
+  _updateInDocs(id, doc) {
+    let beforeId = undefined;
+
+    const existingDoc = this.get(id);
+
+    // Is the order changing?
+    if (this._getProp(doc, 'order') !== this._getProp(existingDoc, 'order')) {
       beforeId = this.getBeforeId(doc);
     }
 
     return super.set(id, doc, beforeId);
   }
 
-  _updateInDocs(id, doc, beforeId) {
-    if (beforeId === undefined) {
-      const existingDoc = this.get(id);
+  move(sourceIndex, destinationIndex, doc) {
+    // Note: the Mapa does not provide sequential indexes so we have to loop through all the items.
+    // In general, we shouldn't be using _move() with large data sets as moving with beforeKey is
+    // far more efficient. If we need to improve performance for larger datasets in the future, we
+    // can store sequential indexes in Mapa.
 
-      // Is the order changing?
-      if (this._getProp(doc, 'order') !== this._getProp(existingDoc, 'order')) {
-        beforeId = this.getBeforeId(doc);
-      }
+    // The endIndex defines the end of the set that needs to be modified
+    let endIndex = null;
+
+    // The beginReorderIndex and endRedorderIndex define the set that needs to be reordered after
+    // the doc is moved
+    let beginReorderIndex = null;
+    let endRedorderIndex = null;
+
+    let docId = null;
+
+    // Are we removing the doc, e.g. archiving it?
+    let newOrder = destinationIndex;
+    if (destinationIndex === null) {
+      // Move it to the end, but set newOrder to null
+      destinationIndex = this.length() - 1;
     }
 
-    return super.set(id, doc, beforeId);
+    const movingDown = destinationIndex > sourceIndex;
+    if (movingDown) {
+      endIndex = destinationIndex;
+      beginReorderIndex = sourceIndex + 1;
+      endRedorderIndex = destinationIndex;
+    } else {
+      endIndex = sourceIndex;
+      beginReorderIndex = destinationIndex;
+      endRedorderIndex = sourceIndex - 1;
+    }
+
+    let beforeKey = movingDown ? undefined : null;
+    let afterKey = movingDown ? null : undefined;
+
+    // Build a list of all the affected entries. This is done before making any updates as the
+    // updates will actually modify the underlying Mapa.
+    const entries = [];
+    let index = 0;
+    for (const entry of this.entries()) {
+      if (index >= beginReorderIndex && index <= endRedorderIndex) {
+        entries.push(entry);
+      }
+
+      if (movingDown) {
+        if (index === destinationIndex) {
+          afterKey = entry[0];
+        }
+      } else if (index === destinationIndex) {
+        beforeKey = entry[0];
+      }
+
+      if (index === sourceIndex) {
+        docId = entry[0];
+        if (doc === undefined) {
+          doc = entry[1];
+        }
+      }
+
+      if (index === endIndex) {
+        // Exit the loop, the rest of the items are unaffected
+        break;
+      }
+
+      index++;
+    }
+
+    // Move the doc
+    this._setProp(doc, 'order', newOrder);
+    super.set(docId, doc, beforeKey, afterKey);
+
+    // Adjust order property of all affected docs
+    entries.forEach((entry, i) => {
+      const order = i + beginReorderIndex + (movingDown ? -1 : 1);
+
+      const item = entry[1];
+      this._setProp(item, 'order', order);
+      const id = entry[0];
+
+      // Use super.set() so that we force item to stay in the same place. This prevents us from
+      // traversing the list to find the correct place to move the item as the order property is
+      // changing
+      super.set(id, item);
+    });
+
+    return doc;
   }
 
-  set(id, doc, beforeId) {
-    if (this.has(id)) {
-      return this._updateInDocs(id, doc, beforeId);
+  _findSourceAndDestinationIndexes(id, newDoc) {
+    // Note: we use a more resilent way than just using the existingOrder and newOrder for the
+    // destinationIndex as race conditions can result in duplicate "order" values
+
+    // const sourceIndex = this._getProp(existingDoc, 'order');
+    // const destinationIndex = this._getProp(newDoc, 'order');
+
+    const newOrder = this._getProp(newDoc, 'order');
+
+    let sourceIndex = null;
+    let destinationIndex = null;
+    let index = 0;
+    for (const entry of this.entries()) {
+      const entryId = entry[0];
+      const entryOrder = this._getProp(entry[1], 'order');
+
+      if (entryId === id) {
+        sourceIndex = index;
+      }
+
+      if (
+        destinationIndex === null &&
+        newOrder !== null &&
+        newOrder <= entryOrder
+      ) {
+        destinationIndex = index;
+      }
+
+      if (sourceIndex !== null && destinationIndex !== null) {
+        // Exit loop as source and destination have been found
+        break;
+      }
+
+      index++;
+    }
+
+    return {
+      sourceIndex,
+      destinationIndex
+    };
+  }
+
+  _updateAndReorderInDocs(id, doc) {
+    const existingDoc = this.get(id);
+
+    const existingOrder = this._getProp(existingDoc, 'order');
+    const newOrder = this._getProp(doc, 'order');
+    if (existingOrder === newOrder) {
+      // Just update the doc as the order isn't changing so we don't need to incur any extra
+      // overhead in reordering
+      return this._updateInDocs(id, doc);
     } else {
-      return this._createInDocs(id, doc, beforeId);
+      const {
+        sourceIndex,
+        destinationIndex
+      } = this._findSourceAndDestinationIndexes(id, doc);
+      return this.move(sourceIndex, destinationIndex, doc);
+    }
+  }
+
+  set(id, doc, beforeId, reorder) {
+    if (beforeId !== undefined) {
+      return super.set(id, doc, beforeId);
+    } else if (this.has(id)) {
+      if (reorder) {
+        return this._updateAndReorderInDocs(id, doc);
+      } else {
+        return this._updateInDocs(id, doc);
+      }
+    } else {
+      if (reorder) {
+        return this._createInDocsAndReorder(id, doc);
+      } else {
+        return this._createInDocs(id, doc);
+      }
     }
   }
 }
--- src/stores/store-mapa.test.js
+++ src/stores/store-mapa.test.js
@@ -170,3 +170,206 @@ it('should archive', () => {
 });
 
 // TODO: move when archived docs
+
+it('should create and reorder', () => {
+  const reorder = true;
+
+  // Initial data
+  let mapa = new StoreMapa();
+  mapa.set('a', { v: 'a', order: 0 }, undefined, reorder);
+  mapa.set('b', { v: 'b', order: 1 }, undefined, reorder);
+  mapa.set('c', { v: 'c', order: 2 }, undefined, reorder);
+
+  // Insert
+  mapa.set('d', { v: 'd', order: 1 }, undefined, reorder);
+  expect(mapa.map(doc => doc)).toEqual([
+    { v: 'a', order: 0 },
+    { v: 'd', order: 1 },
+    { v: 'b', order: 2 },
+    { v: 'c', order: 3 }
+  ]);
+
+  // Create when order is null
+  mapa.set('e', { v: 'e', order: null }, undefined, reorder);
+  expect(mapa.map(doc => doc)).toEqual([
+    { v: 'a', order: 0 },
+    { v: 'd', order: 1 },
+    { v: 'b', order: 2 },
+    { v: 'c', order: 3 },
+    { v: 'e', order: null }
+  ]);
+
+  // Create when order is undefined
+  mapa.set('f', { v: 'f' }, undefined, reorder);
+  expect(mapa.map(doc => doc)).toEqual([
+    { v: 'a', order: 0 },
+    { v: 'd', order: 1 },
+    { v: 'b', order: 2 },
+    { v: 'c', order: 3 },
+    { v: 'e', order: null },
+    { v: 'f' }
+  ]);
+});
+
+it('should create and reorder when duplicate orders', () => {
+  const reorder = true;
+
+  let mapa = new StoreMapa();
+  mapa.set('a', { v: 'a', order: 0 });
+  mapa.set('b', { v: 'b', order: 1 });
+  mapa.set('c', { v: 'c', order: 1 });
+  mapa.set('d', { v: 'd', order: 2 });
+  mapa.set('e', { v: 'e', order: 2 });
+  mapa.set('f', { v: 'f', order: 2 }, undefined, reorder);
+  expect(mapa.map(doc => doc)).toEqual([
+    { v: 'a', order: 0 },
+    { v: 'b', order: 1 },
+    { v: 'c', order: 1 },
+    { v: 'f', order: 2 },
+    { v: 'd', order: 3 },
+    { v: 'e', order: 4 }
+  ]);
+
+  mapa.set('g', { v: 'g', order: 1 }, undefined, reorder);
+  expect(mapa.map(doc => doc)).toEqual([
+    { v: 'a', order: 0 },
+    { v: 'g', order: 1 },
+    { v: 'b', order: 2 },
+    { v: 'c', order: 3 },
+    { v: 'f', order: 4 },
+    { v: 'd', order: 5 },
+    { v: 'e', order: 6 }
+  ]);
+});
+
+it('should move up and reorder', () => {
+  const reorder = true;
+
+  // Initial data
+  let mapa = new StoreMapa();
+  mapa.set('a', { v: 'a', order: 0 });
+  mapa.set('b', { v: 'b', order: 1 });
+  mapa.set('c', { v: 'c', order: 2 });
+  mapa.set('d', { v: 'd', order: 3 });
+
+  // Move up
+  mapa.set('c', { v: 'c', order: 1 }, undefined, reorder);
+  expect(mapa.map(doc => doc)).toEqual([
+    { v: 'a', order: 0 },
+    { v: 'c', order: 1 },
+    { v: 'b', order: 2 },
+    { v: 'd', order: 3 }
+  ]);
+});
+
+it('should move up and reorder when duplicate orders', () => {
+  const reorder = true;
+
+  // Move when duplicate orders
+  let mapa = new StoreMapa();
+  mapa.set('a', { v: 'a', order: 0 });
+  mapa.set('b', { v: 'b', order: 1 });
+  mapa.set('c', { v: 'c', order: 1 });
+  mapa.set('d', { v: 'd', order: 2 });
+  mapa.set('e', { v: 'e', order: 2 });
+  mapa.set('d', { v: 'd', order: 1 }, undefined, reorder);
+  expect(mapa.map(doc => doc)).toEqual([
+    { v: 'a', order: 0 },
+    { v: 'd', order: 1 },
+    { v: 'b', order: 2 },
+    { v: 'c', order: 3 },
+    { v: 'e', order: 2 } // Not touched as considered unaffected
+  ]);
+});
+
+it('should move down and reorder', () => {
+  const reorder = true;
+
+  // Initial data
+  const mapa = new StoreMapa();
+  mapa.set('a', { v: 'a', order: 0 });
+  mapa.set('b', { v: 'b', order: 1 });
+  mapa.set('c', { v: 'c', order: 2 });
+  mapa.set('d', { v: 'd', order: 3 });
+
+  // Move down
+  mapa.set('b', { v: 'b', order: 2 }, undefined, reorder);
+  expect(mapa.map(doc => doc)).toEqual([
+    { v: 'a', order: 0 },
+    { v: 'c', order: 1 },
+    { v: 'b', order: 2 },
+    { v: 'd', order: 3 }
+  ]);
+});
+
+it('should move down and reorder when duplicate orders', () => {
+  const reorder = true;
+
+  // Initial data
+  const mapa = new StoreMapa();
+  mapa.set('a', { v: 'a', order: 0 });
+  mapa.set('b', { v: 'b', order: 1 });
+  mapa.set('c', { v: 'c', order: 1 });
+  mapa.set('d', { v: 'd', order: 2 });
+  mapa.set('e', { v: 'e', order: 2 });
+
+  // Move down
+  mapa.set('b', { v: 'b', order: 2 }, undefined, reorder);
+  expect(mapa.map(doc => doc)).toEqual([
+    { v: 'a', order: 0 },
+    { v: 'c', order: 1 },
+    { v: 'd', order: 2 },
+    { v: 'b', order: 3 },
+    { v: 'e', order: 2 } // Not touched as considered unaffected
+  ]);
+});
+
+it('should update and not reorder when order is not changing', () => {
+  const reorder = true;
+
+  // Initial data
+  const mapa = new StoreMapa();
+  mapa.set('a', { v: 'a', order: 0 });
+
+  // Update
+  const moveSpy = jest.spyOn(mapa, 'move');
+  mapa.set('a', { v: 'a1', order: 0 }, undefined, reorder);
+  expect(mapa.map(doc => doc)).toEqual([{ v: 'a1', order: 0 }]);
+  expect(moveSpy).toHaveBeenCalledTimes(0);
+});
+
+it('should archive and reorder', () => {
+  const reorder = true;
+
+  // Initial data
+  const mapa = new StoreMapa();
+  mapa.set('a', { v: 'a', order: 0 });
+  mapa.set('b', { v: 'b', order: 1 });
+  mapa.set('c', { v: 'c', order: 2 });
+
+  // Archive
+  mapa.set('b', { v: 'b', order: null }, undefined, reorder);
+  expect(mapa.map(doc => doc)).toEqual([
+    { v: 'a', order: 0 },
+    { v: 'c', order: 1 },
+    { v: 'b', order: null }
+  ]);
+});
+
+it('should move with indexes', () => {
+  // Initial data
+  let mapa = new StoreMapa();
+  mapa.set('a', { v: 'a', order: 0 });
+  mapa.set('b', { v: 'b', order: 1 });
+  mapa.set('c', { v: 'c', order: 2 });
+  mapa.set('d', { v: 'd', order: 3 });
+
+  // Move up
+  mapa.move(2, 1);
+  expect(mapa.map(doc => doc)).toEqual([
+    { v: 'a', order: 0 },
+    { v: 'c', order: 1 },
+    { v: 'b', order: 2 },
+    { v: 'd', order: 3 }
+  ]);
+});
