31605f95d1a5fc7c024f7549accf7f83c9e03d50
Geoff Cox <redgeoff@gmail.com>
2018-11-11 20:13:35 -0800

    feat(form-builder): mson property

--- src/form/form-builder.js
+++ src/form/form-builder.js
@@ -1,54 +1,106 @@
-export default {
-  name: 'FormBuilder',
-  component: 'Form',
-  fields: [
-    {
-      name: 'tabs',
-      component: 'Tabs',
-      items: [
+import Form from './form';
+import FormEditor from './form-editor';
+import Tabs from '../tabs';
+import FormField from '../fields/form-field';
+import Set from '../actions/set';
+
+export default class FormBuilder extends Form {
+  _className = 'FormBuilder';
+
+  _create(props) {
+    super._create(props);
+
+    this.set({
+      schema: {
+        component: 'Form',
+        fields: [
+          {
+            name: 'mson',
+            component: 'Field'
+          }
+        ]
+      },
+
+      fields: [
+        new Tabs({
+          name: 'tabs',
+          items: [
+            {
+              name: 'edit',
+              label: 'Edit',
+              icon: 'Edit'
+            },
+            {
+              name: 'preview',
+              label: 'Preview',
+              icon: 'ViewCompact'
+            }
+          ]
+        }),
+
+        new FormField({
+          name: 'form',
+          form: new FormEditor()
+        })
+      ],
+
+      listeners: [
         {
-          name: 'edit',
-          label: 'Edit',
-          icon: 'Edit'
+          event: 'fields.tabs.content.edit',
+          actions: [
+            new Set({
+              name: 'fields.form.form.editable',
+              value: true
+            })
+          ]
         },
+
         {
-          name: 'preview',
-          label: 'Preview',
-          icon: 'ViewCompact'
+          event: 'fields.tabs.content.preview',
+          actions: [
+            new Set({
+              name: 'fields.form.form.editable',
+              value: false
+            })
+          ]
         }
       ]
-    },
+    });
+  }
 
-    {
-      name: 'form',
-      component: 'FormField',
-      form: {
-        component: 'FormEditor'
-      }
+  _setMSON(mson) {
+    const fields = mson.fields.map(field => ({
+      ...field,
+      componentName: field.component
+    }));
+
+    this.get('fields.form.form.fields.fields').setValue(fields);
+  }
+
+  set(props) {
+    super.set({ ...props, mson: undefined });
+
+    if (props.mson !== undefined) {
+      this._setMSON(props.mson);
     }
-  ],
+  }
 
-  listeners: [
-    {
-      event: 'fields.tabs.content.edit',
-      actions: [
-        {
-          component: 'Set',
-          name: 'fields.form.form.editable',
-          value: true
-        }
-      ]
-    },
+  _getMSON() {
+    return {
+      component: 'Form',
+      fields: this.get('fields.form.form.fields.fields').mapForms(form => ({
+        ...form.getValues({ default: false }),
+        component: form.getValue('componentName'),
+        componentName: undefined
+      }))
+    };
+  }
 
-    {
-      event: 'fields.tabs.content.preview',
-      actions: [
-        {
-          component: 'Set',
-          name: 'fields.form.form.editable',
-          value: false
-        }
-      ]
+  getOne(name) {
+    if (name === 'mson') {
+      return this._getMSON();
     }
-  ]
-};
+
+    return super.getOne(name);
+  }
+}
