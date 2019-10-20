7ea24386136dabe79cb8295c73e1f5a86ca57da8
Geoff Cox <redgeoff@gmail.com>
2019-10-20 15:43:01 -0700

    fix(component-form): define in js so can use instanceof

--- src/form/component-form.js
+++ src/form/component-form.js
@@ -1,27 +1,34 @@
-export default {
-  name: 'ComponentForm',
-  component: 'Form',
-  fields: [
-    {
-      component: 'TextField',
-      name: 'name',
-      label: 'Name',
-      required: true,
-      maxLength: 60
-    },
-    {
-      component: 'TextField',
-      name: 'definition',
-      label: 'Definition',
-      required: true
-    }
-  ],
-  access: {
-    form: {
-      create: 'admin',
-      read: 'admin',
-      update: 'admin',
-      archive: 'admin'
-    }
+import Form from './form';
+import TextField from '../fields/text-field';
+
+// Note: we use JS to define the component so that we can use `instanceof ComponentForm`
+export default class ComponentForm extends Form {
+  _className = 'ComponentForm';
+
+  _create(props) {
+    super._create(props);
+
+    const fields = [
+      new TextField({
+        name: 'definition',
+        label: 'Definition',
+        required: true
+      })
+    ];
+
+    // By default, lock down access
+    const access = {
+      form: {
+        create: 'admin',
+        read: 'admin',
+        update: 'admin',
+        archive: 'admin'
+      }
+    };
+
+    this.set({
+      access,
+      fields
+    });
   }
-};
+}
