976a49adb8c6d4d8d5453bd37caee8681d3287f6
Geoff Cox <redgeoff@gmail.com>
2020-05-24 17:14:32 -0700

    feat(generate-component)

---
+++ src/actions/generate-component.js
@@ -0,0 +1,33 @@
+import Action from './action';
+
+export default class GenerateComponent extends Action {
+  _className = 'GenerateComponent';
+
+  _create(props) {
+    super._create(props);
+
+    this.set({
+      schema: {
+        component: 'Form',
+        fields: [
+          {
+            name: 'componentFactory',
+            component: 'Field',
+            required: true,
+          },
+        ],
+      },
+    });
+  }
+
+  _generateForm(factory) {
+    const form = factory.produce();
+    this.set({ form });
+    return form;
+  }
+
+  async act(props) {
+    const factory = this.get('componentFactory');
+    return factory.produce();
+  }
+}
