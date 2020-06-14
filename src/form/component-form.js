0774cbc5f628ce9483bba84cc5af6b752740275b
Geoff Cox <redgeoff@gmail.com>
2020-06-13 17:39:36 -0700

    fix(component-form): support componentName

--- src/form/component-form.js
+++ src/form/component-form.js
@@ -1,6 +1,27 @@
 import Form from './form';
 import Field from '../fields/field';
 
+class ComponentDefinitionField extends Field {
+  // Leave _className with the inherited value of "Field" so that the UI knows how to render (hide)
+  // it. FUTURE: we may want to define something like a HiddenField that the we can use to represent
+  // a Field that should not be rendered.
+  //
+  // _className = 'ComponentDefinitionField';
+
+  _setValue(value) {
+    if (value !== null) {
+      // Also allow for "componentName" as "component" is a MSON keyword and results in compilation
+      value = {
+        ...value,
+        component: value.component ? value.component : value.componentName,
+        componentName: undefined,
+      };
+    }
+
+    super._setValue(value);
+  }
+}
+
 // Note: we use JS to define the component so that we can use `instanceof ComponentForm`
 export default class ComponentForm extends Form {
   _className = 'ComponentForm';
@@ -9,8 +30,10 @@ export default class ComponentForm extends Form {
     super._create(props);
 
     const fields = [
-      // A JSON blob defining the component
-      new Field({
+      // A JSON blob defining the component. We define a ComponentDefinitionField so that we can
+      // dynamically modify the value, to workaround the fact that the "component" keyword cannot be
+      // used in MSON.
+      new ComponentDefinitionField({
         name: 'definition',
         label: 'Definition',
         required: true,
