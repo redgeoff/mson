e396ac58aa4f68319a5a754ac4361a54543bf199
Geoff Cox <redgeoff@gmail.com>
2020-10-06 08:18:21 -0700

    feat(generate-component): dynamic definition

--- src/actions/generate-component.js
+++ src/actions/generate-component.js
@@ -1,4 +1,5 @@
 import Action from './action';
+import compiler from '../compiler';
 
 export default class GenerateComponent extends Action {
   _className = 'GenerateComponent';
@@ -10,9 +11,15 @@ export default class GenerateComponent extends Action {
       schema: {
         component: 'Form',
         fields: [
+          // {
+          //   name: 'componentFactory',
+          //   component: 'Field',
+          //   required: true,
+          // },
           {
-            name: 'componentFactory',
-            component: 'Field',
+            // Use a string instead of object so that we can dynamically change any detail
+            name: 'definition',
+            component: 'TextField',
             required: true,
           },
         ],
@@ -20,8 +27,11 @@ export default class GenerateComponent extends Action {
     });
   }
 
-  async act(props) {
-    const factory = this.get('componentFactory');
-    return factory.produce();
+  async act(/* props */) {
+    // const factory = this.get('componentFactory');
+    // return factory.produce();
+
+    const definition = this.get('definition');
+    return compiler.newComponent(JSON.parse(definition));
   }
 }
