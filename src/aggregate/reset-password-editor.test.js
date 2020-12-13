15103bfa2065dc034461663c31465a14e8a9a402
Geoff Cox <redgeoff@gmail.com>
2020-12-13 15:43:56 -0800

    test(reset-password-editor) (#289)
    * test(reset-password-editor): should auto validate
    
    * test(reset-password-editor): should createRecord
    
    * test(reset-password-editor): should reset password
    
    * test(test-utils): comment out code not yet used

---
+++ src/aggregate/reset-password-editor.test.js
@@ -0,0 +1,95 @@
+import compiler from '../compiler';
+import MemoryStore from '../stores/memory-store';
+import testUtils from '../test-utils';
+
+let acts;
+let editor;
+
+const store = new MemoryStore();
+
+beforeEach(() => {
+  acts = [];
+  editor = compiler.newComponent({
+    component: 'ResetPasswordEditor',
+    store,
+  });
+});
+
+it('should auto validate', async () => {
+  editor.set({ autoValidate: true });
+  editor.getField('password').setValue('secret123');
+  editor.getField('retypePassword').setValue('secret1234');
+  expect(editor.getErrs()).toEqual([
+    { field: 'retypePassword', error: 'must match' },
+  ]);
+});
+
+it('should createRecord', async () => {
+  const resetPassword = jest.fn();
+
+  const mockedRegistrar = {
+    resetPassword: {
+      resetPassword,
+    },
+  };
+
+  testUtils.mockComponentListeners(
+    editor,
+    acts,
+    false,
+    false,
+    false,
+    mockedRegistrar
+  );
+
+  editor.getField('token').setValue('token123');
+
+  await editor.runListeners('createRecord');
+
+  expect(resetPassword).toHaveBeenCalledWith({
+    ...resetPassword.mock.calls[0][0],
+    token: 'token123',
+  });
+});
+
+it('should reset password', async () => {
+  const mockedGlobals = {
+    route: {
+      parameters: {
+        token: 'token123',
+      },
+    },
+  };
+
+  testUtils.mockComponentListeners(editor, acts, false, true, mockedGlobals);
+
+  await editor.runListeners('reset');
+
+  testUtils.expectActsToContain(acts, [
+    {
+      name: 'Set',
+      props: {
+        name: 'fields.token.value',
+        value: 'token123',
+      },
+    },
+    {
+      name: 'UpsertDoc',
+      props: {
+        store,
+      },
+    },
+    {
+      name: 'Snackbar',
+      props: {
+        message: 'Password updated',
+      },
+    },
+    {
+      name: 'Redirect',
+      props: {
+        path: '/',
+      },
+    },
+  ]);
+});
