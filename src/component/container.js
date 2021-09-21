708d703ba35c146025da12a0ffab80d8446f2a1a
Geoff Cox <redgeoff@gmail.com>
2021-09-21 06:11:13 -0700

    feat(container): component to wrap content

---
+++ src/component/container.js
@@ -0,0 +1,17 @@
+const container = {
+  component: 'UIComponent',
+  name: 'Container',
+  schema: {
+    component: 'Form',
+    fields: [
+      {
+        name: 'content',
+        component: 'Field',
+        label: 'Content',
+        docLevel: 'basic',
+      },
+    ],
+  },
+};
+
+export default container;
