0a4c5163689b2f3aa7fdcbf3333ee61fa4008eb7
Geoff Cox <redgeoff@gmail.com>
2022-01-16 14:51:39 -0800

    feat(component): support a condensed notation (#569)
    * feat(component): support a condensed notation
    
    * docs(readme): use condensed class notation

--- src/component/component.test.js
+++ src/component/component.test.js
@@ -14,3 +14,52 @@ it('should concat schemas', () => {
     'three',
   ]);
 });
+
+const shouldSupportComponentNotations = (component, className) => {
+  expect(component.get('isStore')).toEqual(true);
+  expect(component.get('docLevel')).toEqual('basic');
+  expect(component.getClassName()).toEqual(className);
+};
+
+// Legacy notation where className is specified separately
+class LegacyNotationComponent extends Component {
+  className = 'app.LegacyNotationComponent';
+
+  create(props) {
+    super.create(props);
+    this.set({ docLevel: 'basic' });
+  }
+}
+
+it('should support legacy component notation', () => {
+  const component = new LegacyNotationComponent({ isStore: true });
+  shouldSupportComponentNotations(component, 'app.LegacyNotationComponent');
+});
+
+// Condensed notation where everything is in a single create() call
+class CondensedNotationComponent extends Component {
+  constructor(props) {
+    super({
+      name: 'app.CondensedNotationComponent',
+      docLevel: 'basic',
+      ...props,
+    });
+  }
+}
+
+it('should support condensed component notation', () => {
+  const component = new CondensedNotationComponent({ isStore: true });
+  shouldSupportComponentNotations(component, 'app.CondensedNotationComponent');
+});
+
+const createFunctionalNotationComponent = (props) =>
+  new Component({
+    name: 'app.FunctionalNotationComponent',
+    docLevel: 'basic',
+    ...props,
+  });
+
+it('should support functional component notation', () => {
+  const component = createFunctionalNotationComponent({ isStore: true });
+  shouldSupportComponentNotations(component, 'app.FunctionalNotationComponent');
+});
