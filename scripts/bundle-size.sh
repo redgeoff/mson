8651e0e15202041401503e489ea6930e4dc723a1
Geoff Cox <redgeoff@gmail.com>
2022-02-18 15:52:33 -0800

    fix(bundle-size): double quotes for variable values

--- scripts/bundle-size.sh
+++ scripts/bundle-size.sh
@@ -4,6 +4,6 @@ maxBytes=298000
 bundleSize=$(wc -c < dist/mson.js)
 
 if [ ${bundleSize} -gt ${maxBytes} ]; then
-  echo 'Error: bundle (${bundleSize} bytes) is large than ${maxBytes} bytes!'
+  echo "Error: bundle (${bundleSize} bytes) is large than ${maxBytes} bytes!"
   exit 1
 fi
