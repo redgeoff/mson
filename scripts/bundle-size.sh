d6d6d9629c3725a119d115f77733ec422df3c844
Geoff Cox <redgeoff@gmail.com>
2022-02-18 15:48:24 -0800

    feat(bundle-size): output bundle size

--- scripts/bundle-size.sh
+++ scripts/bundle-size.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 maxBytes=298000
+bundleSize=$(wc -c < dist/mson.js)
 
-if [ $(wc -c < dist/mson.js) -gt ${maxBytes} ]; then
-  echo 'Error: bundle too large!'
+if [ ${bundleSize} -gt ${maxBytes} ]; then
+  echo 'Error: bundle (${bundleSize} bytes) is large than ${maxBytes} bytes!'
   exit 1
 fi
